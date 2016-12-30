53bda508fe351edbdfdfc38dd2787c12c86ba524
Cory Kleiser <kleisecj@mail.uc.edu>
2016-12-30 17:04:39 -0500

    added promo section

--- builds/aaronmichael/js/script.js
+++ builds/aaronmichael/js/script.js
@@ -1,13 +1,21 @@
 'use strict';
+
+
 $(document).ready(function () {
 
-    var $viewportWidth = $(window).width();
+    /**
+     * Check viewport width and set data-toggle for navbar accordingly!
+     */
+    function setNavbarToggle() {
+        var $viewportWidth = $(window).width();
 
-    console.log($viewportWidth);
+        console.log($viewportWidth);
 
-    if($viewportWidth < 768){
-        $('ul.nav').children()
-            .attr('data-toggle', 'collapse')
-            .attr('data-target', '#navbar');
+        if ($viewportWidth < 768) {
+            $('ul.nav').children()
+                .attr('data-toggle', 'collapse')
+                .attr('data-target', '#navbar');
+        }
     }
+    setNavbarToggle();
 });
