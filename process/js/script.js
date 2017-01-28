87a97c7d9ece5161e22ad494341f49b8b55a323e
Cory Kleiser <kleisecj@mail.uc.edu>
2017-01-28 15:57:54 -0500

    set minwidth for .tn

--- process/js/script.js
+++ process/js/script.js
@@ -24,12 +24,13 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
     $(window).resize(function(e){
         setNavbarToggle();
+        $('#at4-share3').remove();
+        $('#at-share-dock3').remove();
     });
 
     $(window).on('load', function () {
         $('#at4-share3').remove();
         $('#at-share-dock3').remove();
         console.log('load');
-
     });
 });
